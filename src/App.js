048102e0c18fe95a8e2a7cbbe4c2bc0cb989fa35
rishabh verma <vermarishabh0987@gmail.com>
2020-08-10 23:30:03 +0530

    code editor initial

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
 import React from "react";
-
+import "./App.scss";
+import { Resizable } from "re-resizable";
+import CodeEditor from "./components/codeeditor/codeeditor";
 function App() {
+  const style = {
+    backgroundColor: "#01252f",
+  };
+
   return (
     <div className="App">
-      <h1>heelo</h1>
+      <div className="left"></div>
+      <div className="center">
+        <div className="code-editor">
+          <CodeEditor />
+        </div>
+        <Resizable
+          className="terminal"
+          style={style}
+          defaultSize={{
+            width: "100%",
+            height: 200,
+          }}
+          maxHeight="400"
+          enable={{
+            top: true,
+            right: false,
+            bottom: false,
+            left: false,
+            topRight: false,
+            bottomRight: false,
+            bottomLeft: false,
+            topLeft: false,
+          }}
+          minHeight={30}
+        ></Resizable>
+      </div>
+      <div className="right"></div>
     </div>
   );
 }
---
+++ src/components/codeeditor/codeeditor.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./codeeditor.scss";
+function CodeEditor() {
+  return (
+    <div className="container">
+      <h1>code editor</h1>
+    </div>
+  );
+}
+
+export default CodeEditor;
